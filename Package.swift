7bc617a50a01ab2e6c01469e4e6adbc6404831f0
MrStiv2 <temamax1@gmail.com>
2025-12-18 14:51:25 +0100

    Exclude file from NotificationInjectionGateway target
    Added 'NotificationInjectionGateway.swift' to the exclude list for the NotificationInjectionGateway target in Package.swift to prevent it from being included in the build.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
     targets: [
         .target(
             name: "NotificationInjectionGateway",
-            path: "Sources/NotificationInjectionGateway" 
+            path: "Sources/NotificationInjectionGateway",
+            exclude: ["NotificationInjectionGateway.swift"] 
         ),
         .testTarget(
             name: "NotificationInjectionGatewayTests",
